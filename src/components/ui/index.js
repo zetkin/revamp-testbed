c2a5da04c959942cdc50378872aaca1ff4222348
kristofferlarberg <mail@kristofferlarberg.se>
2020-12-15 09:51:16 +0100

    sancho first draft
    initial commit
    
    finished

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-	  'react-md': require('./ReactMd').default,
+	'react-md': require('./ReactMd').default,
     'fluent-ui': require('./FluentUI').default,
     'grommet-ui': require('./GrommetUI').default,
     'ant-design': require('./AntDesign').default,
     'chakra': require('./Chakra').default,
+    'sancho': require('./Sancho').default,
 };
